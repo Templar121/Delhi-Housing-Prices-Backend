5aa8a3775558f90a48dc73db6d5ca789ac09aba3
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-05-18 12:22:38 +0530

    folder structure updated

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Delhi Housing Prices Backend"
+AUTHOR_USER_NAME = "Templar121"
+SRC_REPO = "houseProject"
+AUTHOR_EMAIL = "subhayanmukherjee78@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="End to End ML backend for housing prices prediction in delhi",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
