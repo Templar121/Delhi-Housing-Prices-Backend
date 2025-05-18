0059aa828fd9c3d01c45e090b1b2ab75c3d7da96
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-05-18 21:41:36 +0530

    data ingestion added

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Delhi Housing Prices Backend"
+REPO_NAME = "Indian Housing Prices Backend"
 AUTHOR_USER_NAME = "Templar121"
 SRC_REPO = "houseProject"
 AUTHOR_EMAIL = "subhayanmukherjee78@gmail.com"
@@ -17,7 +17,7 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="End to End ML backend for housing prices prediction in delhi",
+    description="End to End ML backend for housing prices prediction of India",
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
