ad38a1269d88e2cb1279ef495a20e06e57d410df
Subhayan Mukherjee <subhayanmukherjee78@gmail.com>
2025-05-19 20:38:04 +0530

    model trainer added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from houseProject import logger
 from houseProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from houseProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from houseProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -12,3 +14,22 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/houseProject/config/configuration.py
+++ src/houseProject/config/configuration.py
@@ -1,6 +1,8 @@
 from houseProject.constants import *
 from houseProject.utils.common import read_yaml, create_directories
-from houseProject.entity.config_entity import DataIngestionConfig
+from houseProject.entity.config_entity import (DataIngestionConfig,
+                                               DataValidationConfig,
+                                               DataTransformationConfig)
 
 class ConfigurationManager:
     
@@ -31,3 +33,31 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
+    
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
